a848c9602e514fe17c0149974671b92f7c3c9728
heimzza <hamzassm18@gmail.com>
2022-07-24 20:03:00 +0300

    search

--- src/List.js
+++ src/List.js
@@ -7,13 +7,31 @@ class List extends Component {
     {
         contacts: Proptypes.array.isRequired
     };
+    state={
+        filterText:""
+    }
+    onChangeFilterText= (e) => {
+        // console.log(e.target.value);
+        this.setState({
+            filterText:e.target.value
+        });
+    }
   render() {
+
+    const filteredContacts=this.props.contacts.filter(
+        contact => {
+            return contact.name.toLowerCase().indexOf(
+                this.state.filterText.toLowerCase()
+            ) !== -1
+        }
+    );
+
     return (
       <div className='listArea'>
-        <input name='filter' id='filter' placeholder={"Filter by name or phone"} />
+        <input value={this.state.filterText} onChange={this.onChangeFilterText} name='filter' id='filter' placeholder={"Filter by name or phone"} />
         <ul className='list'>
             {
-                    this.props.contacts.map(contact =>
+                    filteredContacts.map(contact =>
                         <li key={contact.phone}>
                             <span className='name'>{contact.name}</span>
                             <span className='phone'>{contact.phone}</span>
